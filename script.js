5d36c110570a3cc7045140ce47e34420986cfd2d
Felicia Hetman <feli5659@stud.kea.dk>
2022-06-09 16:13:33 +0200

    FORSIDE FÆRDIG

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const navBar = document.querySelector(".main-nav");
-const menuToggleBtn = document.querySelector(".menu-toggle");
+// Lav variablen "btn", der henviser til ".toggle-btn"
+const btn = document.querySelector(".toggle-btn");
+// Lav variablen "menu", der henviser til ".main-menu"
+const menu = document.querySelector(".main-menu");
 
-menuToggleBtn.addEventListener("click", () => {
+// Lav funktionen "toggleMenu()" med følgende funktionalitet
+function toggleMenu() {
+  // a) toggle klassen "shown" på menu vha. classList.toggle
+  menu.classList.toggle("shown");
 
-  menuToggleBtn.classList.toggle("active");
-  navBar.classList.toggle("active");
+  // b) lav variablen "menuShown", som siger, at menu-variablen indeholder klassen "shown" via classList.contains("")
+  const menuShown = menu.classList.contains("shown");
 
-  if (!navBar.classList.contains("active")) {
-    menuToggleBtn.textContent = "MENU";
+  // c) spørg om "menuShown" i if-sætningen nedenfor (=> if (menuShown)), og udskift teksten
+
+  if (menuShown) {
+    console.log(menuShown); // se i konsollen
+    btn.textContent = "LUK";
+    // sæt btn.textContent til "Luk", hvis menuShown er "true"
   } else {
-    menuToggleBtn.textContent = "LUK";
+    console.log(menuShown); // se i konsollen
+    btn.textContent = "MENU";
+    // sæt btn.textContent til "Menu", hvis menuShown er "false"
   }
-});
+  // "toggleMenu()" slutter her
+}
+
+// Tilføj et klik-event til "btn", der sætter toggleMenu-funktionen i gang
+btn.addEventListener("click", toggleMenu);
